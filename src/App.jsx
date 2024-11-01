8d99cb78ef405c882f64a18177b08b3d9cbeef5f
Nahi Abou Nassif <nrabounassif@gmail.com>
2024-11-01 13:50:26 +0200

    Adding Toaster for message popups

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 
 import RouterConfig from './router/routerConfig';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
           <RouterConfig />
+          <Toaster position="top-center" />
     </QueryClientProvider>
   );
 }
