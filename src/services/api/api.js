48b111d845f71e69e4def6142cb2d775c826242a
Nahi Abou Nassif <nrabounassif@gmail.com>
2024-11-01 13:20:23 +0200

    Adding main API configuration

---
+++ src/services/api/api.js
@@ -0,0 +1,39 @@
+import axios from "./axios.config";
+
+//Get All Genres
+export const getGenres = async (token) => {
+    const response = await axios.get("/api/spotify/genres",
+        {headers: { 'Authorization' : `Bearer ${token}` }}
+    );
+    return response.data;
+}
+
+//Get Artists Related to Genre
+export const getArtists = async (token, genre, page) => {
+    const response = await axios.get(`/api/spotify/artists?genre=${genre}&page=${page}`,
+        {headers: { 'Authorization' : `Bearer ${token}` }}
+    );
+    return response.data;
+}
+
+//Register a new user
+export const register = async () => {
+    const response = await axios.get("/api/auth/register");
+    return response.data;
+}
+
+//Get Chat History related to a certain artist
+export const getChatHistory = async (token, artistId) => {
+    const response = await axios.get(`/api/chat/from/${artistId}`,
+        {headers: { 'Authorization' : `Bearer ${token}` }}
+    );
+    return response.data;
+}
+
+//Get Response to a messageg
+export const getResponse = async (token, artistId, message) => {
+    const response = await axios.post(`/api/chat/with/${artistId}`,{ message: message},
+        {headers: { 'Authorization' : `Bearer ${token}` }}
+    );
+    return response.data;
+}
