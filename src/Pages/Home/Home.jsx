cd82779279060d727a650b43838ba6d2e2a566ca
Nahi Abou Nassif <nrabounassif@gmail.com>
2024-11-01 15:25:09 +0200

    Adding ButtonGroup Component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,11 @@
-
+import ButtonGroup from "../../components/ButtonGroup/ButtonGroup";
 const Home = () => {
-    
-    return <div>Home</div>
+    const buttonWrapperStyle = "felx-wrap rounded-md shadow-sm p-4 min-w-[50%] justify-center gap-2";
+    const buttonstyle = "rounded-xl bg-[#0D3D45] px-3 py-2 text-xs border border-[#69D8F7] text-[#69D8F7] hover:bg-[#69D8F7] hover:text-[#0D3D45]  hover:border-[#0D3D45]"
+    const containerStyle = "text-center bg-[#1A1A1A] p-5 rounded-xl m-5 max-w-2xl min-w-lg min-w-64"
+    return <div class="h-screen w-full flex flex-wrap items-center justify-center">
+        <ButtonGroup title="Pick your genre" data={["a","b"]} buttonStyle={buttonstyle} buttonWrapperStyle={buttonWrapperStyle} containerStyle={containerStyle}/>
+    </div>
 
 }
 
---
+++ src/components/ButtonGroup/ButtonGroup.jsx
@@ -0,0 +1,27 @@
+
+import { memo } from 'react';
+
+export default memo(function ButtonGroup({ title, data, buttonStyle, containerStyle, buttonWrapperStyle, onGenreSelected = () => null }) {
+    
+    return (
+      <div class={containerStyle}>
+          {title && <h1 class="text-white">{title}</h1>}
+          <span className={"isolate inline-flex " + buttonWrapperStyle || " rounded-md shadow-sm p-4 min-w-[50%] justify-center gap-2"}>
+            
+            {data && data.map( d => 
+                <button
+                    onClick={() => onGenreSelected()}
+                    type="button"
+                    className={"relative inline-flex " + buttonStyle || "items-center rounded-l-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"}
+                >
+                    {d}
+                </button>
+            )}
+            
+
+        </span>
+      </div>
+      
+    )
+  });
+  
