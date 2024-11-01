aed7b5f3c6ae8944d948b2f5a072e525b0257798
Nahi Abou Nassif <nrabounassif@gmail.com>
2024-11-01 17:53:13 +0200

    Change styling Genre List, scroll bas

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,11 @@
 import ButtonGroup from "../../components/ButtonGroup/ButtonGroup";
 import { useGetGenres } from "../../services/api/apiHooks";
 import { useCookies } from "react-cookie";
-import { toast } from "react-hot-toast";
 
 //Styling
-const buttonWrapperStyle = "felx-wrap rounded-md shadow-sm p-4 min-w-[50%] justify-center gap-2";
+const buttonWrapperStyle = "flex-wrap rounded-md shadow-sm p-4 min-w-[50%] justify-center gap-2";
 const buttonstyle = "rounded-xl bg-[#0D3D45] px-3 py-2 text-xs border border-[#69D8F7] text-[#69D8F7] hover:bg-[#69D8F7] hover:text-[#0D3D45]  hover:border-[#0D3D45]"
-const containerStyle = "text-center bg-[#1A1A1A] p-5 rounded-xl m-5 max-w-2xl min-w-lg min-w-64"
+const containerStyle = "text-center bg-[#1A1A1A] p-5 rounded-xl m-5 max-w-2xl min-w-lg min-w-64 max-h-[60%] overflow-y-scroll"
 
 const Home = () => {
     const [ cookies , ] = useCookies(["authToken"]);
