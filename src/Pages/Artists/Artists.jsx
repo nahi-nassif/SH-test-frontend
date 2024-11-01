b1b5f7c87f1327f7ac83e787cabd70583bb5e49b
Nahi Abou Nassif <nrabounassif@gmail.com>
2024-11-02 01:20:47 +0200

    Finalize Artists List Design

--- src/Pages/Artists/Artists.jsx
+++ src/Pages/Artists/Artists.jsx
@@ -7,6 +7,12 @@ import { toast } from "react-hot-toast";
 import { useCookies } from "react-cookie";
 import ArtistCard from "../../components/ArtistCard/ArtistCard"
 
+//Styling
+const cardStyle = "p-4 mx-auto rounded-xl w-full"
+const titleStyle = "text-base mt-3 mb-1 text-left semi-bold"
+const followerStyle = "text-left text-sm text-[#B3B3B3] font-bold"
+const popularitStyle = "bottom-0 right-4 pb-4 px-1 pt-1 bg-[#0D3D45] text-[#69D8F7] text-sm font-bold rounded-t min-w-[39px]"
+
 const Artists = () => {
     const { genre } = useParams();
     const [ cookies , ] = useCookies(["authToken"]);
@@ -21,16 +27,25 @@ const Artists = () => {
     },[error])
     
     return <div title="Artists List" className="text-white">
-        { data && data.map( d => <div key={d.id}>
-                <ArtistCard 
-                    name = {d.name}
-                    img = {d.images?.length > 0 ? d.images[0].url : ""}
-                    popularity={d.popularity}
-                    followers={d.followers?.total}
-                />
-                {d.name}
-            </div>)
-        }
+        <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5  mt-[10%] mx-[12%]">
+            { data && data.map( d => <li 
+                key={d.id} 
+                className="col-span-1 flex flex-col rounded-xl text-center border border-[#363636] bg-[#1A1A1A] hover:bg-[#363636] hover:scale-105 mx-auto"
+            >
+                    <ArtistCard
+                        name = {d.name}
+                        img = {d.images?.length > 0 ? d.images[0].url : ""}
+                        popularity={d.popularity}
+                        followers={d.followers?.total}
+                        cardStyle={cardStyle}
+                        titleStyle={titleStyle}
+                        followerStyle={followerStyle}
+                        popularitStyle={popularitStyle}
+                    />
+                </li>)
+            }
+        </ul>
+        
     </div>
 
 }
--- src/components/ArtistCard/ArtistCard.jsx
+++ src/components/ArtistCard/ArtistCard.jsx
@@ -2,10 +2,11 @@ import Numeral from 'react-numeral';
 
 const ArtistCard = ({ name, followers, popularity, img, cardStyle, titleStyle, followerStyle, popularitStyle }) => {
 
-    return <div title="card" className={`relative flex flex-1 flex-col max-w-max ${cardStyle}`}>
-            <img alt="artist-image" src={img || "/logo512.png"} className="mx-auto h-[200px] w-[200px]flex-shrink-0 rounded-lg" />
+    return <div title="card" className={`relative flex flex-1 flex-col ${cardStyle}`}>
+            <img alt="artist-image" src={img || "/logo512.png"} className="mx-auto h-auto w-full flex-shrink-0 rounded-lg" />
             <h3 title="name" className={titleStyle}>{name || ""}</h3>
-            <p title="followers"><Numeral value={followers || 0} format={"0,0a"}/> followers</p>
+            <p title="followers" className={followerStyle}><Numeral value={followers || 0} format={"0,0a"}/> followers</p>
+            <span className={'absolute ' + (popularitStyle || "bottom-4 right-4 bg-black")}>{popularity}</span>
     </div>
 
 }
