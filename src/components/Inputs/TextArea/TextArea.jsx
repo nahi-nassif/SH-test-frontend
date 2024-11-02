bd49551853c0a3fb416824d4c115b80ec8e1b67f
Nahi Abou Nassif <nrabounassif@gmail.com>
2024-11-02 10:33:45 +0200

    Adding TextArea Component

---
+++ src/components/Inputs/TextArea/TextArea.jsx
@@ -0,0 +1,35 @@
+import "./style.css"
+
+
+const TextArea = ({ name, maxCharacters, rows, placeholder, placeholderIcon, onChange, onFocus, value,textAreaContainerStyle, textAreaStyle, counterStyle}) => {
+
+    const maxChars = maxCharacters || 3000; // Maximum character limit
+
+    return (
+        <div className={"text-area relative " + textAreaContainerStyle}>
+            <textarea
+                name = {name || "text"}
+                value={value}
+                onChange={onChange}
+                onFocus={onFocus}
+                rows={ rows || 2}
+                maxLength={maxChars} // Set max characters
+                placeholder={placeholder || "Type your message..."}
+                style={{ width: '100%', resize: 'none' }}
+                className={"p-4 placeholder:indent-4 " + textAreaStyle}
+            />
+            {
+                placeholderIcon && <span className="icon absolute top-5 left-2 w-4">
+                    {placeholderIcon}
+                </span>
+            }
+            <div className={"absolute bottom-1 right-0 " + counterStyle}>
+                {value?.length || 0} / {maxChars}
+            </div>
+        </div>
+    )
+
+    
+}
+
+export default TextArea;
