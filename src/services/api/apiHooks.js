007f0b3bf02dcd48ad6f57c334b68f581613bdbb
Nahi Abou Nassif <nrabounassif@gmail.com>
2024-11-02 12:56:59 +0200

    Fix api hooks

--- src/services/api/apiHooks.js
+++ src/services/api/apiHooks.js
@@ -9,22 +9,22 @@ import * as api from "./api";
 export const useGetGenres = (token) => 
   useQuery({
     queryKey: ["genres", token],
-    queryFn: (token) => api.getGenres(token),
+    queryFn: () => api.getGenres(token),
 });
 
 export const useGetArtists = (token, genre, page) => 
   useQuery({
     queryKey: ["artists", {token: token, genre: genre, page: page}],
-    queryFn: (token, genre, page) => api.getArtists(token, genre, page),
+    queryFn: () => api.getArtists(token, genre, page),
 });
 
 export const useGetChatHistory = (token, artistId) => 
   useQuery({
     queryKey: ["chatHistory", {token: token, artistId: artistId}],
-    queryFn: (token, artistId) => api.getChatHistory(token, artistId),
+    queryFn: () => api.getChatHistory(token, artistId),
 });
 
-export const useGetResponse = (token, artistId, message) => {
+export const useGetResponse = () => {
     const queryClient = useQueryClient();
     useMutation({
     mutationFn: (token, artistId, message) => api.getResponse(token, artistId, message),
