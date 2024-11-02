d55aa21b2046f4bed56c3fa71927b3587909dda6
Nahi Abou Nassif <nrabounassif@gmail.com>
2024-11-02 02:44:33 +0200

    Add artist  Card onClick Logic

--- src/Pages/Artists/Artists.jsx
+++ src/Pages/Artists/Artists.jsx
@@ -8,6 +8,8 @@ import { toast } from "react-hot-toast";
 import { useCookies } from "react-cookie";
 import ArtistCard from "../../components/ArtistCard/ArtistCard";
 import Header from "../../components/Header/Header"
+import { useDispatch } from 'react-redux'
+import { setSelectedArtist } from '../../redux/features/selectedArtistSlice'
 
 //Styling
 const cardStyle = "p-4 mx-auto rounded-xl w-full"
@@ -20,13 +22,16 @@ const Artists = () => {
     const [ cookies , ] = useCookies(["authToken"]);
     const token = cookies?.authToken;
     const { data, error } = useGetArtists(token, genre, 1);
+    const dispatch = useDispatch()
 
     const navigate = useNavigate();
 
     const handleGenreClick = () => {
         navigate("/"); // Equivalent to a browser back action
     };
-    
+    const handleCardClick = (selectedArtist) => {
+        dispatch(setSelectedArtist(selectedArtist))
+    }
 
     useEffect(()=>{
         if(error){
@@ -44,6 +49,14 @@ const Artists = () => {
         <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5  mt-[10%] mx-[12%]">
             { data && data.map( d => <li 
                 key={d.id} 
+                onClick={()=>{
+                    handleCardClick({
+                        name: d.name,
+                        img: d.images?.length > 0 ? d.images[0].url : "",
+                        popularity: d.popularity,
+                        followers: d.followers?.total
+                    })
+                }}
                 className="col-span-1 flex flex-col rounded-xl text-center border border-[#363636] bg-[#1A1A1A] hover:bg-[#363636] hover:scale-105 mx-auto"
             >
                     <ArtistCard
